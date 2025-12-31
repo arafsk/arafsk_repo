67b90cdad353dbdc09ba21d913f6c3100cc630d1
arafsk <arafsos@protonmail.com>
2025-12-31 14:58:53 +0000

    update

---
+++ x86_64/update_repo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPO_NAME="arafsk_repo"
+
+rm *.db *.db.sig *.db.tar.gz *.db.tar.gz.sig *.files *.files.sig *.files.tar.gz *.files.tar.gz.sig
+
+sleep 1
+
+echo "repo-add"
+
+repo-add --verify --sign -R $REPO_NAME.db.tar.gz *.pkg.tar.zst
+
+#repo-add $REPO_NAME.db.tar.gz *.pkg.tar.zst
+
+echo "####################################"
+echo "Repo Updated!!"
+echo "####################################"
